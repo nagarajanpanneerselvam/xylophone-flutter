abf0756ab2eeaf8ea3d49830277db9f8c0689b9e
Nagarajan Panneerselvam <nagarajan.pselvam@thoughtworks.com>
2022-07-19 00:38:37 +0530

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,46 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  play(int soundId) {
+    print('Playing $soundId');
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundId.wav'));
+  }
+
+  Widget getExpandWiget({Color color, int noteID}) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          play(noteID);
+        },
+        child: Container(
+          color: color,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              getExpandWiget(color: Colors.red, noteID: 1),
+              getExpandWiget(color: Colors.orange, noteID: 2),
+              getExpandWiget(color: Colors.yellow, noteID: 3),
+              getExpandWiget(color: Colors.green, noteID: 4),
+              getExpandWiget(color: Colors.green.shade900, noteID: 5),
+              getExpandWiget(color: Colors.blue, noteID: 6),
+              getExpandWiget(color: Colors.purple, noteID: 7),
+            ],
+          ),
         ),
       ),
     );
